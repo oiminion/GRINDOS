0bea8ec198cc20319a33de8daa50392bce890df8
oiminion <arthurkoichi@gmail.com>
2025-04-15 17:38:35 -0300

    acabei

--- MainComponent/global.gd
+++ MainComponent/global.gd
@@ -2,12 +2,13 @@ extends Node
 
 signal Points_Changed
 
-var process_colors: Array = PackedColorArray([
+var process_colors: PackedColorArray = PackedColorArray([
 	Color(0.69675,0.31901,0.42888),
 	Color(0.60407,0.94785,0.50633),
 	Color(0.34241,0.36421,0.31082),
 	Color(0.28623,0.99492,0.77138),
 	Color(0.05499,0.47307,0.05812),
+	
 	Color(0.13976,0.97430,0.05900),
 	Color(0.44925,0.71712,0.22794),
 	Color(0.97270,0.18832,0.60954),
@@ -35,17 +36,17 @@ var points: int = 0:
 		points = value
 		Points_Changed.emit()
 
-var RAM_max_capacity: float = 200
+var RAM_max_capacity: float = 500
 
 var disk_max_capacity: float = 200
 
 var unlocked_upgrades: Dictionary = {}
 
 func get_Process_Color() -> Color:
-	var aux :int = randi_range(0,10)
+	var aux :int = randi_range(0,9)
 	while(used_process_colors[aux]):
 		aux += 1
-		if aux == 10:
+		if aux >= 9:
 			aux = 0
 	
 	used_process_colors[aux] = true
@@ -54,6 +55,6 @@ func get_Process_Color() -> Color:
 func free_Process_Color(process_color: Color) -> void:
 	var aux :int = 0
 	print(process_color)
-	while( process_color[aux] != process_color):
+	while( process_colors[aux] != process_color):
 		aux += 1
 	used_process_colors[aux] = false
