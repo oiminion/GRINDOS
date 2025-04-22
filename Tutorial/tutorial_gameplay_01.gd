50a5e965d2988d8b149a3261d7de48897996e5f8
Eidiyoshi <eidiyoshi@hotmail.com>
2025-04-22 17:43:11 -0300

    fudeu

--- Tutorial/tutorial_gameplay_01.gd
+++ Tutorial/tutorial_gameplay_01.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+
+@export var Selected: Module = null
+
+@export var segmentation_size: float = 50
+
 @export var tutorial_progress : int = 0
 var ended: bool = false
 
@@ -26,3 +31,19 @@ func _on_character_layer_finished_talk() -> void:
 			ended = true
 	if not ended:
 		$character_layer.setStringArray(array)
+
+
+func _on_so_so_selected(module: Module) -> void:
+	if Selected == null:
+		Selected = module
+	elif Selected.get_parent() != module.get_parent():
+		$Cable.Connect_Module(module, Selected)
+	print("A")
+
+
+func _on_ram_fixo_module_selected(module: Module) -> void:
+	if Selected == null:
+		Selected = module
+	elif Selected.get_parent() != module.get_parent():
+		$Cable.Connect_Module(module, Selected)
+ 
